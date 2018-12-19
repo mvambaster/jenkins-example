d5d9cebf4452c42d8937b079ef733c3ed33a4343
Michael Vambaster <45100957+mvambaster@users.noreply.github.com>
2018-12-19 09:56:01 -0200

    Update FizzBuzzTest.java

--- src/test/java/com/techprimers/testing/FizzBuzzTest.java
+++ src/test/java/com/techprimers/testing/FizzBuzzTest.java
@@ -36,8 +36,7 @@ class FizzBuzzTest {
     @Test
     public void testZero() {
 
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> {fB.play(0)});
+        Assertions.assertThrows(IllegalArgumentException.class,() -> fB.play(0));
     }
 
     @AfterEach
